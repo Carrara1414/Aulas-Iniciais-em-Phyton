4761b18e4de1915cc58868b7d688c12c88e0fedd
João_Carrara <68128341+Carrara1414@users.noreply.github.com>
2025-02-18 22:51:37 -0300

    Add files via upload

---
+++ aula8.py
@@ -0,0 +1,8 @@
+nome_completo = "João Gabriel Souza Carrara"
+idade = 21
+ano_nascimento = 2003
+altura = 1.78
+maioridade = 18
+teste_maioridade = idade >= maioridade
+
+print("Seu nome é:",nome_completo, ".\n","Ele tem:", idade, "anos.","\n", "Ele mede:", altura, "centimetros.","\n","Ele é maior de idade?:", teste_maioridade,",","pois nasceu em", ano_nascimento,".")
